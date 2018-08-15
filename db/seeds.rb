10d2f7101557904b3fdb041735d03a7ab680988a
Gianpaolo DAmato <damatohg@gmail.com>
2018-08-15 18:03:03 -0400

    compelted component message form

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Message.create(author: "Evil Martian", text: "Surrender!")
+Message.create(author: "Heroe Martian", text: "No!")
--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,4 +1,6 @@
 import "init";
 import "components/page/page";
-import "./application.css";
-import "./hello_react.jsx";
+import "components/auth-form/auth-form";
+import "components/chat/chat";
+// import "./application.css";
+// import "./hello_react.jsx";
